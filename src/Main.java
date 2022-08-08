ae3440be833ff74525e47725c0b1744bc13441fa
Денис Шестернин <dshesternin@gmail.com>
2022-08-09 00:25:18 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.text.DecimalFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        BmiService bmi = new BmiService();
+        DecimalFormat dF = new DecimalFormat("#.##");
+       double bodyMass = bmi.calculate(180,1.84);
+        System.out.println("ИМТ равен " + dF.format(bodyMass) + " кг/м2");
+    }
+}
