3d83fd2849d570a8d1b3213c95612f7d4f8c30d5
Денис Шестернин <dshesternin@gmail.com>
2022-08-13 01:02:19 +0300

    Commit reformat code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService bmi = new BmiService();
         DecimalFormat dF = new DecimalFormat("#.##");
-       double bodyMass = bmi.calculate(180,1.84);
+        double bodyMass = bmi.calculate(180, 1.84);
         System.out.println("ИМТ равен " + dF.format(bodyMass) + " кг/м2");
     }
 }
